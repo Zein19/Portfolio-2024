7266b53e204c359e839018d26e3e51a83ae90683
Zein19 <zein.23@hotmail.co.uk>
2024-01-31 15:51:46 +0000

    updated navbar responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,89 @@
 import { ArrowRightIcon } from "@heroicons/react/solid";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Navbar() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
+        <div className="flex items-center">
+          <a href="#about" className="title-font font-medium text-white mb-4 md:mb-0 ml-3 text-xl">
             Zein Sharif
           </a>
-        </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
-            Projects
-          </a>
-          <a href="#skills" className="mr-5 hover:text-white">
-            Skills
-          </a>
-        </nav>
-        <div class="flex">
-            <a href="https://twitter.com/zeinsharif_" target="_blank" rel="noopener noreferrer" class="text-white-500 hover:text-blue-700 mx-3">Twitter</a>
-            <a href="https://www.linkedin.com/in/zeinsharif/" rel="noopener noreferrer" target="_blank" class="text-white-500 hover:text-blue-700 mx-3">LinkedIn</a>
-            <a href="https://github.com/Zein19" target="_blank" rel="noopener noreferrer" class="text-white-500 hover:text-blue-700 mx-3">GitHub</a>
+          <button
+            className="md:hidden text-white ml-2"
+            onClick={toggleMobileMenu}
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
+          </button>
         </div>
-        <a  
-            href="Zein-Sharif-SD-CV.pdf"
-            download="ZeinSharif-CV"           
-            className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
-            CV
+        <nav
+          className={`md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center ${
+            mobileMenuOpen ? "block" : "hidden md:flex"
+          }`}
+        >
+            <a href="#projects" className="mr-5 hover:text-white block md:inline-block md:mr-0 md:ml-5 mx-3 mb-2 md:mb-0">
+              Projects
+            </a>
+            <a href="#skills" className="mr-5 hover:text-white block md:inline-block md:mr-0 md:ml-5 mx-3 mb-2 md:mb-0">
+              Skills
+            </a>
+        </nav>
+        <div className={`md:flex ${mobileMenuOpen ? "block" : "hidden"}`}>
+          <a
+            href="https://twitter.com/zeinsharif_"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white-500 hover:text-blue-700 block md:inline-block md:mr-0 md:ml-5 mx-3 mb-2 md:mb-0"
+          >
+            Twitter
+          </a>
+          <a
+            href="https://www.linkedin.com/in/zeinsharif/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="text-white-500 hover:text-blue-700 block md:inline-block md:mr-0 md:ml-5 mx-3 mb-2 md:mb-0"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://github.com/Zein19"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white-500 hover:text-blue-700 block md:inline-block md:mr-0 md:ml-5 mx-3 mb-2 md:mb-0"
+          >
+            GitHub
+          </a>
+        </div>
+        <a
+          href="Zein-Sharif-SD-CV.pdf"
+          download="ZeinSharif-CV"
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0 md:mr-5"
+        >
+          CV
         </a>
         <a
           href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+        >
           Hire Me
           <ArrowRightIcon className="w-4 h-4 ml-1" />
         </a>
